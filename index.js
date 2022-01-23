a9e9011a4b5e1a864fcb1fc7afba5055a2d0c519
5465869 <css010@uark.edu>
2022-01-23 14:47:59 -0600

    add stuff

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ var cors = require('cors')
 const http = require('http');
 const socket = require('socket.io');
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 var app = express();
 app.use(cors())
 const server = http.createServer(app)
 const io = socket(server)
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 
 
 io.on('connection', function (socket) {
